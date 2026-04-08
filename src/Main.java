cc5cdb9cc0e603a5b6ee460ceb95105bae130a02
alzahara <shahdaljabri33@gmail.com>
2026-04-08 14:52:28 +0400

    Task 16

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+ public static void main (String[] args){
+  System.out.println(" Task 16: only14 ");
+int[] a16 = {1, 4, 1, 4};
+System.out.print("Array {1, 4, 1, 4}: ");
+  only14 test = null;
+  if (test.only14(a16)) { System.out.println("True"); } else { System.out.println("False"); }
+
+
+
+
+
+
+
+
+ }
+}

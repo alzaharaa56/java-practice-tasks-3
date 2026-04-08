6c664b7641e6f9e39e5c9ad1f18a35b73e410072
alzahara <shahdaljabri33@gmail.com>
2026-04-08 15:00:10 +0400

    Task 17

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
  public static void main (String[] args){
-  System.out.println(" Task 16: only14 ");
-int[] a16 = {1, 4, 1, 4};
-System.out.print("Array {1, 4, 1, 4}: ");
-  only14 test = null;
-  if (test.only14(a16)) { System.out.println("True"); } else { System.out.println("False"); }
+
+ // System.out.println(" Task 16: only14 ");
+//int[] a16 = {1, 4, 1, 4};
+//System.out.print("Array {1, 4, 1, 4}: ");
+ // only14 test = null;
+  //if (test.only14(a16)) { System.out.println("True"); } else { System.out.println("False"); }
+
+     System.out.println(" Task 17: isEverywhere ");
+     int[] a17 = {1, 2, 1, 3};
+     System.out.print("Array {1, 2, 1, 3}, val 1: ");
+     isEverywhere test = null;
+     if (test.isEverywhere(a17, 1)) { System.out.println("True"); } else { System.out.println("False"); }
+
+
 
 
 
---
+++ src/isEverywhere.java
@@ -0,0 +1,12 @@
+public class isEverywhere {
+
+    public static boolean isEverywhere(int[] nums, int val) {
+for (int i = 0; i < nums.length - 1; i++) {
+
+if (nums[i] != val && nums[i + 1] != val) {
+return false;
+}
+}
+return true;
+    }
+}
